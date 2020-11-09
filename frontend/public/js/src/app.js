ab07a72a3a548af589a7efa1ac8f8215d68695d5
Matthias <matthias.hiltpold@elca.ch>
2020-11-09 11:28:56 +0100

    refactured backend and app

--- frontend/public/js/src/app.js
+++ frontend/public/js/src/app.js
@@ -1,68 +1,10 @@
 import {select} from "../contrib/d3-selection/src/index";
 
-import dataFromUrls from "./csv";
 import { createLineage } from "./lineage";
 
 import {viewWidth, viewHeight, username, password} from "./config";
-import fetchAsync from "./backend";
+import fetchAsync, {fetchDataFromCsvFiles, csvFiles, makeUrl, makeQueryString, dslEndpoint, lineageEndpoint} from "./backend";
 
-
-const dslQueryParams = {
-  typeName: `sdl_action`,
-  limit: 100
-}
-
-const dslEndpoint = "http://localhost:8080/api/atlas/v2/search/dsl?";
-const lineageEndpoint = "http://localhost:8080/api/atlas/v2/lineage/";
-
-const makeQueryString = (query) => {
-  if(typeof query === "object" && query !== null) {
-    return Object.keys(query).map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(query[key])).join("&");
-  } else {
-    return ""
-  }
-}
-
-const makeUrl = (endpoint, queryString) => {
-  return endpoint+queryString
-}
-
-const dslQuery = makeUrl(dslEndpoint, makeQueryString(dslQueryParams)); 
-
-console.log(dslQuery);
-
-fetchAsync(dslQuery, username, password).then((data) => {
-  console.log(data.entities);
-  const guids = data.entities.map((x) => x.guid);
-  const lineage = guids.map((guid) => {
-    return fetchAsync(lineageEndpoint+guid, username, password);
-  });
-  return Promise.all(lineage);
-}).then((lineages) =>{
-  console.log(lineages);
-  
-  const tmpLineage = lineages[1];
-  const lu = Object.keys(tmpLineage.guidEntityMap).reduce((acc, id) => {
-    acc[id]= tmpLineage.guidEntityMap[id].attributes.qualifiedName;
-    return acc;
-  }, {});
-  console.log(lu);
-  const nodeIds = Object.keys(tmpLineage.guidEntityMap);
-  const nodes = nodeIds.map((id) => { 
-    return {name: lu[id] }
-  });
-
-  const links = tmpLineage.relations.map((relation) => {
-    return { source: lu[relation.fromEntityId], target: lu[relation.toEntityId], value: 1 }
-  });
-  console.log(nodes);
-  console.log(links);
-
-  createLineage(nodes, links, sankeyChart);
-  
-}).catch((error) => {
-  console.log(error);
-});
 
 const rootElement = select("#sdl-lineage-container");
 
@@ -77,19 +19,68 @@ const sankeyChart = svg
   .attr("height", viewHeight)
   .attr("width", viewWidth);
 
-const loadData = dataFromUrls;
 
-Promise.all(loadData).then((data) => {
-  const nodes = data[0].map( node => ({name: node.name, group: node.group}));
-  const links = data[1].map( link => ({source: link.source, target: link.target, value: 1 }));
-  /*
-  console.log("< NODES >");
-  console.log(nodes);
-  console.log("< LINKS >");
-  console.log(links);
-  */
-  //createLineage(nodes, links, sankeyChart);
+const dslQueryParams = {
+  typeName: `sdl_action`,
+  limit: 100
+}
 
-}).catch((error) => {
-  console.log(error);
-});
+const dslQuery = makeUrl(dslEndpoint, makeQueryString(dslQueryParams)); 
+
+console.log(dslQuery);
+
+const createLineageFromAtlas = () => {
+  fetchAsync(dslQuery, username, password).then((data) => {
+    console.log(data.entitiedataFromUrlss);
+    const guids = data.entities.map((x) => x.guid);
+    const lineage = guids.map((guid) => {
+      return fetchAsync(lineageEndpoint+guid, username, password);
+    });
+    return Promise.all(lineage);
+  }).then((lineages) =>{
+    console.log(lineages);
+    
+    const tmpLineage = lineages[1];
+    const lu = Object.keys(tmpLineage.guidEntityMap).reduce((acc, id) => {
+      acc[id]= tmpLineage.guidEntityMap[id].attributes.qualifiedName;
+      return acc;
+    }, {});
+    console.log(lu);
+    const nodeIds = Object.keys(tmpLineage.guidEntityMap);
+    const nodes = nodeIds.map((id) => { 
+      return {name: lu[id] }
+    });
+
+    const links = tmpLineage.relations.map((relation) => {
+      return { source: lu[relation.fromEntityId], target: lu[relation.toEntityId], value: 1 }
+    });
+    console.log(nodes);
+    console.log(links);
+
+    createLineage(nodes, links, sankeyChart);
+    
+  }).catch((error) => {
+    console.log(error);
+  });
+} 
+
+const createLineageFromCsvFiles = () => {
+  console.log(fetchDataFromCsvFiles(csvFiles));
+
+  Promise.all(fetchDataFromCsvFiles(csvFiles)).then((data) => {
+    const nodes = data[0].map( node => ({name: node.name, group: node.group}));
+    const links = data[1].map( link => ({source: link.source, target: link.target, value: 1 }));
+    
+    //console.log("< NODES >");
+    //console.log(nodes);
+    //console.log("< LINKS >");
+    //console.log(links);
+
+    createLineage(nodes, links, sankeyChart);
+  }).catch((error) => {
+    console.log(error);
+  });
+}
+
+//createLineageFromAtlas();
+createLineageFromCsvFiles();
--- frontend/public/js/src/backend.js
+++ frontend/public/js/src/backend.js
@@ -1,4 +1,33 @@
 
+import {csvParse} from "../contrib/d3-dsv/src/index"
+
+export const csvFiles = ["http://localhost:8080/data/vertices_grouped.csv", "http://localhost:8080/data/edges_grouped.csv"];
+
+export const dslEndpoint = "http://localhost:8080/api/atlas/v2/search/dsl?";
+export const lineageEndpoint = "http://localhost:8080/api/atlas/v2/lineage/";
+
+export const fetchCsvAsync = async (url) => {
+    let response = await fetch(url);
+    let data = await response.text();
+    let parsedData = await csvParse(data);
+    return parsedData;
+}
+
+export const fetchDataFromCsvFiles = (files) => {
+    return files.map(async url => await fetchCsvAsync(url));
+}
+
+export const makeQueryString = (query) => {
+  if(typeof query === "object" && query !== null) {
+    return Object.keys(query).map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(query[key])).join("&");
+  } else {
+    return ""
+  }
+}
+
+export const makeUrl = (endpoint, queryString) => {
+  return endpoint+queryString
+}
 export const fetchAsync = async (url, username, password) => {
     const newHeaders = new Headers({
         "Authorization": `Basic ${window.btoa(`${username}:${password}`)}`,
